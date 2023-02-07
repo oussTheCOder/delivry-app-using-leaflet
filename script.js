308cd7d4354f2f7fed73cbff1d86323b4114001e
Ousscoder92 <90870793+Ousscoder92@users.noreply.github.com>
2023-02-07 13:49:37 +0100

    completing some styling

--- script.js
+++ script.js
@@ -1,13 +1,82 @@
 
+// 
+const menuBtn=document.querySelector(".menu-btn");
+const navigation=document.querySelector(".nav-list");
+let menuOpen=false;
+menuBtn.addEventListener("click",showNav);
+/*
+
+
+
+/*-------not done---------
+3-complete calculate costs functionality.
+    //1-create the form.done
+    //2-styling the form.done
+
+    ---complete the functionality of the form.???
+       
+        
+
+    
+
+*/
+/*
+4-complete all the functionalities concerning the map 
+5-adding all the static content of the website.
+6-responsiveness.
+7-make it simple and to the point.
+*/
+
+function showNav(){
+    navigation.classList.toggle("show");
+    if(!menuOpen){
+       menuBtn.classList.add("open");
+       document.body.style.overflowY="hidden";
+       menuOpen=true;
+    }else{
+        menuBtn.classList.remove("open");
+        document.body.style.overflowY="visible";
+        menuOpen=false;
+    }
+}
+// remove the menu when clicking outside the ul list menu
+navigation.addEventListener("click",hideNav);
+function hideNav(event){
+  let target=event.target;
+  if(!target.closest(".nav-list.show ul")){
+    menuBtn.classList.remove("open");
+    navigation.classList.toggle("show");
+    document.body.style.overflowY="visible";
+    menuOpen=false;
+  }
+}
+
+// slider
+let slider = document.querySelector('.slider');
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,4000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active-slide');
+  currentSlide = (currentSlide+1)%slides.length;
+  slides[currentSlide].classList.add('active-slide');
+}
+
+
+
+// map 
+const sectionHome=document.querySelector("section.home-page");
+const sectionMap=document.querySelector("section.map");
 const form=document.querySelector(".map-form");
 const mapArea=document.querySelector(".map-area");
-const boxMapDesc=document.querySelector(".map-description_box");
 const boxStartingState=document.querySelector(".starting-state");
 const boxEndingState=document.querySelector(".ending-state");
 const boxConfirmDist=document.querySelector('.confirm-dist-state');
-const btnSelecDist=document.querySelector(".select-dis_btn");
+const btnCalculateCost=document.querySelector(".calculate_btn");
 const boxCurrentPos=document.querySelector(".actual-pos_box");
 const boxDefinePos=document.querySelector(".define-pos_box");
+const suggestContainerParent=document.querySelector(".sugges-Contain-Parent");
 const suggestionsContainer = document.querySelector(".suggested-pos");
 const btnCloseForm=document.querySelector(".close-form");
 const btnValidateStart=document.querySelector(".validate-start_btn");
@@ -19,6 +88,7 @@ let addressCity;
 let addressDescr;
 let address;
 let markers=[];
+let distinationCoords;
 let latlngs=[];
 let marker;
 
@@ -30,16 +100,15 @@ const  appear=function(element){
     element.classList.remove("hide");
     element.classList.add("show");
 }
-btnSelecDist.addEventListener("click",(event)=>{
-    hide(mapArea);
-    hide(boxMapDesc);
-    appear(form);
+btnCalculateCost.addEventListener("click",(event)=>{
+    sectionMap.classList.add("show-map");
+    appear(mapArea);
+    // appear(form);
     inputStartPos.value="current position";
-    inputEndPos.focus();
+    // inputEndPos.focus();
 })
 btnValidateStart.addEventListener("click",(event)=>{
     hide(mapArea);
-    hide(boxMapDesc);
     hide(boxStartingState);
     appear(form);
 })
@@ -51,16 +120,19 @@ if(navigator.geolocation){//if browser support geolocation
       const {latitude,longitude}=position.coords;//use destructuring to get latitude and longitude of your position
       const coords=[latitude,longitude];
       //creating a map object
-      const map = L.map('myMap').setView(coords, 15);//myMap string is the is where we display our map
+      const map = L.map('myMap').setView(coords, 13);//myMap string is the is where we display our map
       //creating a layer object
       const layer=L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     });
       //adding layer to the map object.
       layer.addTo(map);
+      markers.push()
+      console.log(markers)
       // Attach an event listener to the input field
           document.getElementById("autosuggestEnd").addEventListener("input", function(event) {
             let text = event.target.value;
+            this.autocomplete="off";/*prevent the default dropDown of presvious typed options from appearing */
             getSuggestions(text);
           });
           document.getElementById("autosuggestStart").addEventListener("input",function(event){
@@ -80,7 +152,7 @@ if(navigator.geolocation){//if browser support geolocation
                 const suggestedCityNames=data.suggestions.map(suggestion=>suggestion.text);
                 suggestedCityNames.map((city)=>{
                   const suggestedItem=document.createElement("p");
-                  suggestedItem.textContent=city;
+                  suggestedItem.innerHTML=`<i class="fa-solid fa-location-dot"></i><p>${city}</p>`
                   suggestionsContainer.appendChild(suggestedItem);
                   })
               }catch(error)
@@ -119,9 +191,14 @@ if(navigator.geolocation){//if browser support geolocation
              }
              marker.bindPopup(`${addressDescr}-${addressCity}`,popUpOptions).openPopup();
              markers.push(marker);
+             markers.splice(1,markers.length-2);
+             console.log(markers);
+
              const[marker1,marker2]=markers;
              latlngs=[marker1.getLatLng(),marker2?.getLatLng()||"not created yet"];
-             return latlngs;
+             console.log(marker2?.getLatLng()||"not created yet");
+             const bounds=L.latLngBounds([marker1.getLatLng(),marker2?.getLatLng()]);
+             map.fitBounds(bounds);
  }
  //get the place name and create a marker when the page is loaded
   getPlaceName(latitude,longitude);
@@ -132,9 +209,7 @@ if(navigator.geolocation){//if browser support geolocation
       const response=await fetch(url);
       const result=await response.json();
       const{x:lng,y:lat}=result.locations[0].feature.geometry;
-      console.log(lng,lat);
-      createMarker(lat,lng);
-      return latlngs;
+      await createMarker(lat,lng);
     }
 
   //implement getting position from input place name how
@@ -142,13 +217,11 @@ if(navigator.geolocation){//if browser support geolocation
     let target=event.target.closest(".suggested-pos p");
     if(!target) return;
     inputEndPos.value=target.textContent;
-    getCoordsOfplace(inputEndPos.value);
-    // getCoordsOfplace(inputEndPos.value).then(latlng=>{
-    // let bounds=L.latLngBounds(latlng);
-    // map.fitBounds(bounds);
-    // });
-    //fit the map view to fit the both markers
-    handleBoxDefineClick();
+    suggestionsContainer.innerHTML="";
+    getCoordsOfplace(target.textContent);
+    appear(mapArea);
+    hide(form);
+    appear(boxConfirmDist)
   }
   suggestionsContainer.addEventListener("click",handleSuggesClick);
 
@@ -156,7 +229,7 @@ if(navigator.geolocation){//if browser support geolocation
     const handleClickMap=function(mapEvent){//when click on the map execute this function
       if(boxDefinePos.classList.contains("start-pos")){
         map.removeLayer(marker);//remove the previous marker
-        markers.pop();
+          markers.pop();
           const{lat,lng}=mapEvent.latlng;//get the current latitude and longitude
           getPlaceName(lat,lng);
           createMarker(lat,lng);
@@ -164,12 +237,12 @@ if(navigator.geolocation){//if browser support geolocation
         numClickEndPos++;
         if(numClickEndPos>1){
           map.removeLayer(marker);//remove the previous marker 
-          markers.pop();
-          console.log(markers);
+          // markers.pop();
         }
         const{lat,lng}=mapEvent.latlng;//get the current latitude and longitude
         getPlaceName(lat,lng);
         createMarker(lat,lng);
+        console.log(markers);
       }
     }
     // ----------------------------------------------------------------
@@ -179,7 +252,6 @@ if(navigator.geolocation){//if browser support geolocation
     } 
     const handleBoxDefineClick=function(event){
         hide(form);
-        hide(boxMapDesc);
         appear(mapArea);
         if(boxDefinePos.classList.contains("start-pos")){
           appear(boxStartingState);
@@ -226,15 +298,17 @@ if(navigator.geolocation){//if browser support geolocation
       //handle all events when the user click on inputEndPos...
       inputEndPos.addEventListener("focus",(event)=>{
         boxDefinePos.classList.remove("start-pos");
+        appear(suggestContainerParent);
         boxCurrentPos.removeEventListener("click",handleBoxCurrentPosClick);
         boxDefinePos.addEventListener("click",handleBoxDefineClick);
         map.on("click",handleClickMap);
       })
+      inputEndPos.addEventListener("blur",(e)=>{
+        hide(suggestContainerParent);
+      })
 
       btnCloseForm.addEventListener("click",(event)=>{
-        hide(form);
-        appear(mapArea);
-        appear(boxMapDesc);
+        sectionMap.classList.remove("show-map");
         map.off("click",handleClickMap);
       })
 
